5cc32c819f5abf6fff31ad1383bd56bfa48d023e
Mr-Ye-Cao <caoye_2020@yeah.net>
2023-07-23 14:30:35 -0500

    add mongoose. delete edge runtime. connect to mongodb

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
 
-export const runtime = 'edge'
+import {connectDB} from './db/db'
+
+// export const runtime = 'edge'
 
 export default function IndexPage() {
   const id = nanoid()
+  const db = connectDB()
 
   return <Chat id={id} />
 }
