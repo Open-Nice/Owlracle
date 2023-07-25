71b146332fea229a058166679612f460508ce902
Mr-Ye-Cao <caoye_2020@yeah.net>
2023-07-25 18:11:16 -0500

    dynamic retrieve vectordb based on prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -27,6 +27,7 @@ if (!supabaseServiceKey) throw new Error(`Expected env var SUPABASE_SERVICE_KEY`
 
 
 const getBetterQuery = async (messages: ChatCompletionRequestMessage[]): Promise<string> => {
+
   if (messages.length == 1)
       return messages[0].content!
 
@@ -49,7 +50,7 @@ const getBetterQuery = async (messages: ChatCompletionRequestMessage[]): Promise
       For example, if user asks "What is his
       name?" Then you should figure out what
       does "his" refer to and give me the more
-      complete version of user prompt."
+      complete version of user prompt.
       Don't leave any reference in your eventual answer.
     `}
 
@@ -79,6 +80,76 @@ const getBetterQuery = async (messages: ChatCompletionRequestMessage[]): Promise
   return result.choices[0].message.content
 }
 
+interface RelevantDBResult {
+  // A dictionary indicating if the db is needed
+  dbs: Record<string, boolean>
+  // How many databases are needed
+  dbNum: number
+}
+
+const getRelaventDB = async (query: string): Promise<Promise<RelevantDBResult>> => {
+  const prompt = codeBlock`
+    ${oneLine`
+      You are given four databases:
+      Courses, which contains all the course information at Rice University;
+      Events, which contains all the information of events held by organizations at Rice University;
+      Organizations, which contains all the organization information;
+      Faculties, which contains all the information of professors, graduate students, and other faculties at Rice University.
+      You will then be provided with a question. 
+      Your task is to give all the relevant databases that are necessary to use to answer the question.
+      You have to give as many databases as possible to achieve full coverage.
+      Respond in the following format, do not include any explanation, only the name of the database:
+      database_1
+      database_2
+      ...
+    `}
+
+    This is the question:
+    ${query}
+  `
+
+  const chatMessage : ChatCompletionRequestMessage = {
+    role: 'user',
+    content: prompt,
+  }
+
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [chatMessage],
+    temperature: 0.7,
+  })
+
+  if (!response.ok) {
+    const error = await response.json()
+    console.log('getBetterQuery: Failed to generate completion', error)
+    throw new Error('getBetterQuery: Failed to generate completion')
+  }
+  
+  const result = await response.json()
+  const relaventDBs : string = result.choices[0].message.content.toLowerCase()
+  // console.log(relaventDBs)
+
+  let dbs: Record<string, boolean> = {
+    'courses': false,
+    'events': false,
+    'faculties': false,
+    'organizations': false,
+  };
+  
+  let dbNum = 0
+
+  for (const key in dbs)
+    if (relaventDBs.includes(key.toLowerCase())) {
+      dbs[key] = true;
+      dbNum += 1
+    }
+  
+  return {
+      dbs,
+      dbNum,
+  }
+}
+
 export async function POST(req: Request) {
   const json = await req.json()
   const { messages, previewToken } = json
@@ -98,8 +169,12 @@ export async function POST(req: Request) {
   const betterQuery = await getBetterQuery(messages)
   console.log("Better query:", betterQuery)
 
-  // Create embedding from query
+  // Create embedding for query
   const queryEmbedding = await embeddingFn.embedQuery(betterQuery.replaceAll('\n', ' '));
+
+  // Infer which vectorDB(s) are needed
+  const { dbs, dbNum } = await getRelaventDB(betterQuery)
+  console.log("getRelaventDB:", dbs, dbNum)
 
   // Connect to vector DB
   const supabaseClient = createClient(
@@ -111,31 +186,38 @@ export async function POST(req: Request) {
         autoRefreshToken: false,
       },
     })
-
+  
   // Similarity search
-  const { error: matchError, data: pageSections } = await supabaseClient.rpc(
-    'match_faculties',
-    {
-      query_embedding: queryEmbedding,
-      // match_threshold: 0.78,
-      match_count: 10,
-      // min_content_length: 50,
+  let combinedPageSections = []
+  for (const key in dbs) {
+    if (dbs[key]) {
+      const { error: matchError, data: pageSections } = await supabaseClient.rpc(
+        `match_${key}`,
+        {
+          query_embedding: queryEmbedding,
+          // match_threshold: 0.78,
+          match_count: Math.floor(10 / dbNum),
+          // min_content_length: 50,
+        }
+      )
+      if (matchError) {
+        console.log('Failed to match page sections', matchError)
+        throw new Error(`Failed to match page sections`)
+      }
+
+      combinedPageSections.push(...pageSections)
     }
-  )
-  if (matchError) {
-    console.log('Failed to match page sections', matchError)
-    throw new Error(`Failed to match page sections`)
-  } else {
-    // console.log('Retreival successful:', pageSections)
   }
+  // console.log('Retreival successful:', combinedPageSections)
+
 
   // Generate contexts
   const tokenizer = new GPT3Tokenizer({ type: 'gpt3' })
   let tokenCount = 0
   let contextText = ''
 
-  for (let i = 0; i < pageSections.length; i++) {
-    const pageSection = pageSections[i]
+  for (let i = 0; i < combinedPageSections.length; i++) {
+    const pageSection = combinedPageSections[i]
     const content = pageSection.content
     const encoded = tokenizer.encode(content)
     tokenCount += encoded.text.length
@@ -154,8 +236,8 @@ export async function POST(req: Request) {
 
     ${oneLine`
       Use the above information, answer the following question accurately.
-      Include in your answer as many relevant professors mentioned above as possible.
-      Along with the professor, provide the professors' urls from the above information.
+      Include in your answer as many relevant information mentioned above as possible.
+      Provide relavent url inside your answer.
       If you are unsure and the answer is not explicitly written in the documentation, say
       "Sorry, I don't know how to help with that." Try to make your response concise and
       informative. Try to summarize the information you are given instead of completely copying.
@@ -165,7 +247,7 @@ export async function POST(req: Request) {
     ${betterQuery}
     """
 
-    Answer as markdown. Provide professor's title, websites, email, description, description if available:
+    Answer as markdown.
   `
   // console.log('prompt:', prompt)
 
