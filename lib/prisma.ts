79c71b95c32ee464fb35634d0ff6cf06356326cf
Mr-Ye-Cao <caoye_2020@yeah.net>
2023-07-31 16:46:03 -0500

    make prisma.js typesafe

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,8 @@
-import { PrismaClient } from "@prisma/client/edge"
+import { PrismaClient } from "@prisma/client"
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -6,16 +10,12 @@ import { PrismaClient } from "@prisma/client/edge"
 // Learn more: 
 // https://pris.ly/d/help/next-js-best-practices
 
-let prisma: PrismaClient
-
 if (process.env.VERCEL_ENV === 'production') {
-  prisma = new PrismaClient()
+  global.prisma = new PrismaClient()
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient()
   }
-  prisma = global.prisma
 }
 
-
-export default prisma
+export default global.prisma
