01165909a124014140e390f5e33322366256ff85
Mr-Ye-Cao <caoye_2020@yeah.net>
2023-08-02 12:54:16 -0500

    edit front page

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,16 +6,20 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'Looking for some class? 📚',
+    message: `Give me some philosophy of mind class?`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'Connect with some faculties? 🤠',
+    message: `Find me some professors in machine leanring?`
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: "Find interesting stuff to do 🎮",
+    message: `What are some interesting activities recently?`
+  },
+  {
+    heading: "Join interesting organizations 🎨",
+    message: `what design clubs are there?`
   }
 ]
 
@@ -24,18 +28,20 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          This is Athena!
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
+          An Open Source large language model developed by{' '}
+          <ExternalLink href="https://github.com/Open-Nice">
+            <span style={{ color: '#FF0000' }}>N</span>
+            <span style={{ color: '#FFA500' }}>i</span>
+            <span style={{ color: '#00FF00' }}>c</span>
+            <span style={{ color: '#0000FF' }}>e</span>
           </ExternalLink>
-          .
+          {' '}for Rice<span style={{ fontSize: '25px' }}>🦉</span>s
         </p>
         <p className="leading-normal text-muted-foreground">
-          You can start a conversation here or try the following examples:
+          Here are some interesting questions to get started:
         </p>
         <div className="mt-4 flex flex-col items-start space-y-2">
           {exampleMessages.map((message, index) => (
