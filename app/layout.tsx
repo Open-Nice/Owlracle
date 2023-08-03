d8936f381b1c2844e28edf8ee20e4b15a2e6662f
Mr-Ye-Cao <caoye_2020@yeah.net>
2023-08-02 21:56:36 -0500

    upload logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,9 @@ export const metadata: Metadata = {
     { media: '(prefers-color-scheme: dark)', color: 'black' }
   ],
   icons: {
-    // icon: '/favicon.ico',
-    // shortcut: '/favicon-16x16.png',
-    // apple: '/apple-touch-icon.png'
+    icon: '/logo.ico',
+    shortcut: '/logo.png',
+    apple: '/logo.png'
   }
 }
 
