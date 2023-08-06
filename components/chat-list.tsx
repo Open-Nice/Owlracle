00982cd2dad5d7302d47ff74e4024be9ced478eb
Mr-Ye-Cao <caoye_2020@yeah.net>
2023-08-05 21:00:40 -0500

    show recommend question after chats

--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -2,6 +2,7 @@ import { type Message } from 'ai'
 import { type UseChatHelpers } from 'ai/react'
 import { Separator } from '@/components/ui/separator'
 import { ChatMessage } from '@/components/chat-message'
+import RelatedQuestionArea from "./related-questions"
 
 export interface ChatList {
   isLoading: Boolean
@@ -18,12 +19,18 @@ export function ChatList({ isLoading, messages, setInput }: ChatList) {
     <div className="relative mx-auto max-w-2xl px-4">
       {messages.map((message, index) => (
         <div key={index}>
-          <ChatMessage isComplete = {! (isLoading && index === messages.length - 1)} message={message} setInput = {setInput}/>
+          <ChatMessage isComplete = {! (isLoading && index === messages.length - 1)} message={message}/>
           {index < messages.length - 1 && (
             <Separator className="my-4 md:my-8" />
           )}
         </div>
       ))}
+      {
+        ! isLoading && setInput ?
+          <RelatedQuestionArea setInput = {setInput}/>
+        :
+        <></>
+      }
     </div>
   )
 }
